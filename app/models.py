0cac488c4341936295beed09147856f89e08d0e8
kar288 <kar288@gmail.com>
2014-02-01 17:17:34 +0100

    question

--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import User
+
+class Student(User):
+	jid = models.CharField(max_length = 40);
+	lname = models.CharField(max_length = 100);
+	college = models.CharField(max_length = 100);
+	room = models.CharField(max_length = 6);
+	phone = models.CharField(max_length = 100);
+	country = models.CharField(max_length = 100);
+	majorinfo = models.CharField(max_length = 100);
