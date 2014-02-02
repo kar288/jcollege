8688f9430c187ddcdc889c44da06cc9116464cf8
kar288 <kar288@gmail.com>
2014-02-02 20:33:12 +0100

    analytics

--- jcollege/static/js/whoisjack.js
+++ jcollege/static/js/whoisjack.js
@@ -4,7 +4,7 @@ $(document).ready(function(){
 	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-	ga('create', 'UA-47726965-1', 'whoisjack.herokuapp.com');
+	ga('create', 'UA-47727668-1', 'whoisjack.herokuapp.com');
 	ga('send', 'pageview');
 
 	$('.question-form').submit(submitAnswer)
