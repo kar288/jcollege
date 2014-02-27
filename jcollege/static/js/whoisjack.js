2a732368ca60f390fb186860ae3f362d1d33c04f
dhasegan <danielhasegan@gmail.com>
2014-02-27 14:01:43 -0500

    Added timer

--- jcollege/static/js/whoisjack.js
+++ jcollege/static/js/whoisjack.js
@@ -66,3 +66,22 @@ function newQuestion() {
 		frm.submit(submitAnswer);
 	}); 
 }
+var target_date = new Date('Mar, 3, 2014').getTime();
+var countdown = $('#timer');
+
+setInterval(function () {
+    // find the amount of "seconds" between now and target
+    var current_date = new Date().getTime();
+    var seconds_left = (target_date - current_date) / 1000;
+ 
+    // do some time calculations
+    hours = parseInt(seconds_left / 3600);
+    seconds_left = seconds_left % 3600;
+     
+    minutes = parseInt(seconds_left / 60);
+    seconds = parseInt(seconds_left % 60);
+     
+    // format countdown string + set tag value
+    countdown.html( hours + ' <b>Hours</b> ' + minutes + ' <b>Minutes</b> ' + seconds + ' <b>Seconds</b> ');
+ 
+}, 1000);
